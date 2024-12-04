22528a8c10c6f7d96d2aaad24ecd7a801dd00553
Gaute Berge <gaute.berge@gmail.com>
2024-12-04 12:14:16 +0100

    day 4 cleanup

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -2,29 +2,25 @@ import java.awt.Point
 import java.io.File
 
 operator fun Point.plus(other: Point): Point = Point(x + other.x, y + other.y)
-
 fun List<Point>.getWord(input: List<String>) = mapNotNull { input.getOrNull(it.x)?.getOrNull(it.y) }.joinToString("")
+fun List<String>.getPoints() = indices.flatMap { x -> this[x].indices.map { y -> Point(x, y) } }
 
 fun part1(input: List<String>): Int {
     val vectors = listOf(Point(0, 1), Point(1, 0), Point(1, 1), Point(1, -1))
-
-    fun searchFrom(p: Point, v: Point): Boolean {
-        val word = listOf(p, p + v, p + v + v, p + v + v + v).getWord(input)
-        return word == "XMAS" || word == "SAMX"
+    return input.getPoints().sumOf { p ->
+        vectors.count { v ->
+            val word = listOf(p, p + v, p + v + v, p + v + v + v).getWord(input)
+            word == "XMAS" || word == "SAMX"
+        }
     }
-
-    return input.indices.flatMap { x -> input[x].indices.map { y -> Point(x, y) } }
-        .sumOf { p -> vectors.count { v -> searchFrom(p, v) } }
 }
 
 fun part2(input: List<String>): Int {
-    fun check(p: Point): Boolean {
+    return input.getPoints().count { p ->
         val word1 = listOf(p + Point(-1, -1), p, p + Point(1, 1)).getWord(input)
         val word2 = listOf(p + Point(1, -1), p, p + Point(-1, 1)).getWord(input)
-        return (word1 == "MAS" || word1 == "SAM") && (word2 == "MAS" || word2 == "SAM")
+        (word1 == "MAS" || word1 == "SAM") && (word2 == "MAS" || word2 == "SAM")
     }
-    return input.indices.flatMap { x -> input[x].indices.map { y -> x to y } }
-        .count { (x, y) -> check(Point(x, y)) }
 }
 
 fun main() {
