bb218ab8251e3de8f52a49c0b20750ac1704fb8a
Gaute Berge <gaute.berge@gmail.com>
2024-12-06 22:32:20 +0100

    day 6 cleanup

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -2,58 +2,58 @@ package day6
 
 import java.awt.Point
 import java.io.File
+import kotlin.time.measureTimedValue
 
 operator fun Point.plus(other: Point): Point = Point(x + other.x, y + other.y)
 fun Point.rotate() = Point(y, -x)
 operator fun List<String>.get(p: Point) = this[p.y][p.x]
-fun direction(guard: Char) = when (guard) {
-    '<' -> Point(-1, 0)
-    '>' -> Point(1, 0)
-    'v' -> Point(0, -1)
-    '^' -> Point(0, 1)
-    else -> error("invalid guard")
-}
+
+private val directionVectors = mapOf(
+    '<' to Point(-1, 0),
+    '>' to Point(1, 0),
+    'v' to Point(0, -1),
+    '^' to Point(0, 1),
+)
 
 fun part1(grid: List<String>): Int {
     val points = grid.flatMapIndexed { y, row -> row.mapIndexed { x, _ -> Point(x, y) } }
-    var position = points.find { grid[it] in listOf('<', '>', '^', 'v') }!!
+    var position = points.find { grid[it] in directionVectors }!!
+    var direction = directionVectors[grid[position]]!!
     val visited = mutableSetOf<Point>()
-    var currentDirection = direction(grid[position])
     while (true) {
         visited += position
-        val ahead = position + currentDirection
+        val ahead = position + direction
         if (grid.getOrNull(ahead.y)?.getOrNull(ahead.x) == null) return visited.size
-        if (grid[ahead] == '#') currentDirection = currentDirection.rotate()
-        position += currentDirection
+        if (grid[ahead] == '#') direction = direction.rotate()
+        position += direction
     }
 }
 
 fun wouldCycle(startPosition: Point, startDirection: Point, newObstacle: Point, grid: List<String>): Boolean {
     var position = startPosition
-    var currentDirection = startDirection
+    var direction = startDirection
     val visited = mutableSetOf<Pair<Point, Point>>()
     while (true) {
-        if ((position to currentDirection) in visited) return true
-        visited.add(position to currentDirection)
-        val ahead = position + currentDirection
+        if ((position to direction) in visited) return true
+        visited.add(position to direction)
+        val ahead = position + direction
         if (grid.getOrNull(ahead.y)?.getOrNull(ahead.x) == null) return false
         if (grid[ahead] == '#' || ahead == newObstacle) {
-            currentDirection = currentDirection.rotate()
+            direction = direction.rotate()
             continue
         }
-        position += currentDirection
+        position += direction
     }
 }
 
 fun part2(grid: List<String>): Int {
     val points = grid.flatMapIndexed { y, row -> row.mapIndexed { x, _ -> Point(x, y) } }
-    val start = points.find { grid[it] in listOf('<', '>', '^', 'v') }!!
-    var position = start
-    var currentDirection = direction(grid[position])
+    val start  = points.find { grid[it] in directionVectors }!!
+    val direction = directionVectors[grid[start]]!!
     val obstructionSites = mutableSetOf<Point>()
-    points.forEach { point ->
-        if (point != start && wouldCycle(position, currentDirection, newObstacle = point, grid = grid)) {
-            obstructionSites.add(point)
+    points.forEach { candidate ->
+        if (candidate != start && wouldCycle(start, direction, newObstacle = candidate, grid = grid)) {
+            obstructionSites.add(candidate)
         }
     }
     return obstructionSites.size
@@ -61,10 +61,7 @@ fun part2(grid: List<String>): Int {
 
 
 fun main() {
-    val gridSample = File("input/day6sample.txt").readLines().reversed()
-    part1(gridSample).also { println(it) }
-    part2(gridSample).also { println(it) }
     val grid = File("input/day6.txt").readLines().reversed()
     println(part1(grid)) // 5080
-    println(part2(grid)) // 1919
+    println(measureTimedValue { part2(grid) }) // 1919, 3.6s
 }
