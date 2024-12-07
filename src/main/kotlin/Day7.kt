7d39867fc3436d238fb7285bd4078566a3e00c51
Gaute Berge <gaute.berge@gmail.com>
2024-12-07 14:14:42 +0100

    day 7 without pretty list of operators

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -3,18 +3,24 @@ import util.print
 import java.io.File
 import kotlin.time.measureTime
 
-fun List<Long>.isMatchFrom(target: Long, current: Long, i: Int, ops: List<(Long, Long) -> Long>): Boolean {
+fun List<Long>.isMatchFrom(target: Long, current: Long, i: Int): Boolean {
     if (i == size) return current == target
-    return ops.any { op -> isMatchFrom(target, op(current, this[i]), i + 1, ops) }
+    return isMatchFrom(target, current * this[i], i + 1) ||
+            isMatchFrom(target, current + this[i], i + 1)
+}
+
+fun List<Long>.isMatchFrom2(target: Long, current: Long, i: Int): Boolean {
+    if (i == size) return current == target
+    return isMatchFrom2(target, current * this[i], i + 1) ||
+            isMatchFrom2(target, current + this[i], i + 1) ||
+            isMatchFrom2(target, current * this[i].orderOf10() + this[i], i + 1)
 }
 
 fun main() = measureTime {
     val input = File("input/day7.txt").readLines()
         .map { it.split(":").map { it.trim().split(" ") }.let { it[0][0].toLong() to it[1].map(String::toLong) } }
-    val ops = listOf<(Long, Long) -> Long>(Long::plus, Long::times)
-    val part1 = input.sumOf { (target, nums) -> target.takeIf { nums.isMatchFrom(target, nums[0], 1, ops) } ?: 0 }
-    val ops2 = ops + listOf { a, b -> a * (b.orderOf10()) + b }
-    val part2 = input.sumOf { (target, nums) -> target.takeIf { nums.isMatchFrom(target, nums[0], 1, ops2) } ?: 0 }
+    val part1 = input.sumOf { (target, nums) -> target.takeIf { nums.isMatchFrom(target, nums[0], 1) } ?: 0 }
+    val part2 = input.sumOf { (target, nums) -> target.takeIf { nums.isMatchFrom2(target, nums[0], 1) } ?: 0 }
     println(part1) // 267566105056
     println(part2) // 116094961956019
-}.print()
+}.print() // 55ms
