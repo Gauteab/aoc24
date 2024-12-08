6642dcd57dea9907b0d3f4c985585a3a1e5306b6
Gaute Berge <gaute.berge@gmail.com>
2024-12-08 10:35:21 +0100

    day 8 part 2

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -13,14 +13,21 @@ fun main() {
     val input = File("input/day8.txt").readLines().map { it.toList() }
     val groups = input.flatMapIndexed { y, row -> row.mapIndexed { x, c -> c to Point(x, y) } }.groupBy { it.first }.filter { it.key !in ".#" }
     val antinodes = mutableSetOf<Point>()
+    fun addAlongVector(start : Point, v:Point) {
+        var p = start
+        while (input[p] != null) {
+            antinodes.add(p)
+            p += v
+        }
+    }
     groups.forEach { (k, antennas) ->
         antennas.forEach { (_, a) ->
             antennas.forEach { (_, b) ->
                 if (a != b) {
                     val v = a - b
                     val (l, r) = if (a + v == b) (a to b) else (b to a)
-                    antinodes.add(l - v)
-                    antinodes.add(r + v)
+                    addAlongVector(l, Point(0,0) - v)
+                    addAlongVector(r, v)
                 }
             }
         }
