5da68f0415da35e5a584b99463d26ce0bc323dc9
Gaute Berge <gaute.berge@gmail.com>
2024-12-08 10:33:45 +0100

    day 8 part 1

---
+++ src/main/kotlin/Day8.kt
@@ -0,0 +1,29 @@
+package day8
+
+import java.awt.Point
+import java.io.File
+import kotlin.math.abs
+
+operator fun Point.plus(other: Point): Point = Point(x + other.x, y + other.y)
+operator fun Point.minus(other: Point): Point = Point(x - other.x, y - other.y)
+operator fun List<List<Char>>.get(p: Point) = getOrNull(p.y)?.getOrNull(p.x)
+
+
+fun main() {
+    val input = File("input/day8.txt").readLines().map { it.toList() }
+    val groups = input.flatMapIndexed { y, row -> row.mapIndexed { x, c -> c to Point(x, y) } }.groupBy { it.first }.filter { it.key !in ".#" }
+    val antinodes = mutableSetOf<Point>()
+    groups.forEach { (k, antennas) ->
+        antennas.forEach { (_, a) ->
+            antennas.forEach { (_, b) ->
+                if (a != b) {
+                    val v = a - b
+                    val (l, r) = if (a + v == b) (a to b) else (b to a)
+                    antinodes.add(l - v)
+                    antinodes.add(r + v)
+                }
+            }
+        }
+    }
+    println(antinodes.filter { input[it] != null }.size)
+}
