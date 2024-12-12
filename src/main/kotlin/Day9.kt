5e2ee4bc3230ba9febe96fb98310c5fa1bec606f
Gaute Berge <gaute.berge@gmail.com>
2024-12-12 12:46:05 +0100

    day 9 part 2

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,14 +1,11 @@
 import java.io.File
 import kotlin.math.abs
 
-fun main() {
+
+fun part1(): Long {
     val input = File("input/day9.txt").readText().toList().map { it.digitToInt() }
     val memory = input.flatMapIndexed { i, n ->
-        if (i % 2 == 0) {
-            (0 until n).map { i / 2 }
-        } else {
-            (0 until n).map { -1 }
-        }
+        if (i % 2 == 0) (0 until n).map { i / 2 } else (0 until n).map { -1 }
     }.toMutableList()
     var i = 0
     var j = memory.size - 1
@@ -22,7 +19,43 @@ fun main() {
             memory[j--] = -1
         }
     }
-    val checksum: Long = memory.mapIndexed { i, v -> if (v < 0L) 0L else i.toLong() * v.toLong() }.sum()
-    // 1368861652 - overflow :(
-    println(checksum) // 6310675819476
+    return memory.mapIndexed { i, v -> if (v < 0L) 0L else i.toLong() * v.toLong() }.sum()
+}
+
+sealed interface Block {
+    var size: Int
+}
+
+data class FileBlock(val id: Long, override var size: Int) : Block
+data class FreeBlock(override var size: Int) : Block
+
+fun part2(): Long {
+    val allocations = File("input/day9.txt").readText().toList().mapIndexed { i, d ->
+        if (i % 2 == 0) FileBlock(i / 2L, d.digitToInt()) else FreeBlock(d.digitToInt())
+    }.toMutableList()
+    var j = allocations.size - 1
+    while (j >= 0) {
+        if (allocations[j] !is FileBlock) {
+            j--
+            continue
+        }
+        val i = allocations.indexOfFirst { it is FreeBlock && it.size >= allocations[j].size }
+        if (i < j && i > -1) {
+            allocations[i].size -= allocations[j].size
+            allocations.add(i, allocations[j])
+            allocations[j + 1] = FreeBlock(allocations[j + 1].size)
+        }
+        j--
+    }
+
+    val memory = allocations
+        .filter { it is FileBlock || it.size > 0 }.toMutableList()
+        .flatMap { block -> (1..block.size).map { if (block is FileBlock) block.id else -1 } }
+    val checksum: Long = memory.mapIndexed { i, v -> if (v < 0L) 0L else i.toLong() * v }.sum()
+    return checksum
+}
+
+fun main() {
+    println(part1()) // 6310675819476
+    println(part2()) // 6335972980679
 }
