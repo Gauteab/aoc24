1fea96aacb4f1eb67efbbcb3498d4034b2aec609
Gaute Berge <gaute.berge@gmail.com>
2024-12-15 11:16:57 +0100

    day 11 part 2

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,6 +1,7 @@
 package day11
 
 import util.print
+import java.io.File
 
 fun Long.applyRule() = when {
     this == 0L -> listOf(1L)
@@ -13,44 +14,38 @@ fun Long.applyRule() = when {
 }
 
 fun main() {
-    var list = listOf(0L)
-    val cache = mutableMapOf<Int, List<Long>>()
-    repeat(40) {
-        list = list.flatMap { it.applyRule() }
-        cache[it + 1] = list
+    val cache = (0..9).map { mutableMapOf<Int, List<Long>>() }
+    val cacheSize = 37
+    (0..9).forEach { i ->
+        var list = listOf(i.toLong())
+        repeat(cacheSize) {
+            list = list.flatMap { it.applyRule() }
+            cache[i][it + 1] = list
+        }
+        println("cache $i built")
     }
-    println("cache built")
 
-//    val input = File("input/day11.txt").readLines()[0].split(' ').map { it.toLong() }
-    val input = listOf(125L, 17L)
+    val input = File("input/day11.txt").readLines()[0].split(' ').map { it.toLong() }
     fun countStones(value: Long, count: Long, blinksLeft: Int): Long {
         if (blinksLeft == 0) return count
-        return when {
-            value == 0L -> {
-                if (blinksLeft <= cache.size)
-                    cache[blinksLeft]!!.size.toLong() + count - 1
-                else
-                    countStones(1L, count, blinksLeft - 1)
-            }
+        if (value < 10L && blinksLeft <= cacheSize) {
+            return cache[value.toInt()][blinksLeft]!!.size.toLong() + count - 1
+        }
 
-            value.toString().length % 2 == 0 -> {
-                val s = value.toString()
-                val a = s.take(s.length / 2).toLong()
-                val b = s.drop(s.length / 2).toLong()
-                val newCount = countStones(a, count + 1, blinksLeft - 1)
-                countStones(b, newCount, blinksLeft - 1)
-            }
-
-            else -> {
-                countStones(value * 2024, count, blinksLeft - 1)
-            }
+        return if (value == 0L) countStones(1L, count, blinksLeft - 1)
+        else if (value.toString().length % 2 == 0) {
+            val s = value.toString()
+            val a = s.take(s.length / 2).toLong()
+            val b = s.drop(s.length / 2).toLong()
+            val newCount = countStones(a, count + 1, blinksLeft - 1)
+            countStones(b, newCount, blinksLeft - 1)
+        } else {
+            countStones(value * 2024, count, blinksLeft - 1)
         }
     }
+
     input.sumOf {
-        println(it)
         countStones(it, 1, 75)
-    }.print()
-//
-    println()
+    }.print() // 220566831337810
 
 }
